b41eb988aea4ea740324560d407d7ebf40d83d88
duyhoang36 <138024073+duyhoang36@users.noreply.github.com>
2024-02-26 11:44:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,433 @@
+var questions = [
+    
+    {
+        type: "trueFalse",
+        question: "Câu hỏi 1: Phép tính này: 2+2 =4 đúng hay sai?",
+        answer: true
+    },
+    {
+        type: "trueFalse",
+        question: "Câu hỏi 2: Phép tính này: 2+2 =4 đúng hay sai?",
+        answer: true
+    },
+    {
+        type: "trueFalse",
+        question: "Câu hỏi 3: Phép tính này: 2+2 =4 đúng hay sai?",
+        answer: true
+    },
+    {
+        type: "trueFalse",
+        question: "Câu hỏi 4: Phép tính này: 2+2 =4 đúng hay sai?",
+        answer: true
+    },
+    {
+        type: "trueFalse",
+        question: "Câu hỏi 5: Phép tính này: 2+2 =4 đúng hay sai?",
+        answer: true
+    },
+    {
+        type: "trueFalse",
+        question: "Câu hỏi 6: Phép tính này: 2+2 =4 đúng hay sai?",
+        answer: true
+    },
+    {
+        type: "trueFalse",
+        question: "Câu hỏi 7: Phép tính này: 2+2 =4 đúng hay sai?",
+        answer: true
+    },
+    {
+        type: "trueFalse",
+        question: "Câu hỏi 8: Phép tính này: 2+2 =4 đúng hay sai?",
+        answer: true
+    },
+    {
+        type: "trueFalse",
+        question: "Câu hỏi 9: Phép tính này: 2+2 =4 đúng hay sai?",
+        answer: true
+    },
+    {
+        type: "trueFalse",
+        question: "Câu hỏi 10: Phép tính này: 2+2 =4 đúng hay sai?",
+        answer: true
+    },
+    {
+        type: "multipleChoice",
+        question: "Câu hỏi 11: Ai là người đầu tiên lên mặt trăng?",
+        choices: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "CAlan Shepard"],
+        answer: "Neil Armstrong"
+    },
+    {
+        type: "multipleChoice",
+        question: "Câu hỏi 12: Ai là người đầu tiên lên mặt trăng?",
+        choices: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "CAlan Shepard"],
+        answer: "Neil Armstrong"
+    },
+    {
+        type: "multipleChoice",
+        question: "Câu hỏi 13: Ai là người đầu tiên lên mặt trăng?",
+        choices: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "CAlan Shepard"],
+        answer: "Neil Armstrong"
+    },
+    {
+        type: "multipleChoice",
+        question: "Câu hỏi 14: Ai là người đầu tiên lên mặt trăng?",
+        choices: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "CAlan Shepard"],
+        answer: "Neil Armstrong"
+    },
+    {
+        type: "multipleChoice",
+        question: "Câu hỏi 15: Ai là người đầu tiên lên mặt trăng?",
+        choices: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "CAlan Shepard"],
+        answer: "Neil Armstrong"
+    },
+    {
+        type: "multipleChoice",
+        question: "Câu hỏi 16: Ai là người đầu tiên lên mặt trăng?",
+        choices: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "CAlan Shepard"],
+        answer: "Neil Armstrong"
+    },
+    {
+        type: "multipleChoice",
+        question: "Câu hỏi 17: Ai là người đầu tiên lên mặt trăng?",
+        choices: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "CAlan Shepard"],
+        answer: "Neil Armstrong"
+    },
+    {
+        type: "multipleChoice",
+        question: "Câu hỏi 18: Ai là người đầu tiên lên mặt trăng?",
+        choices: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "CAlan Shepard"],
+        answer: "Neil Armstrong"
+    },
+    {
+        type: "multipleChoice",
+        question: "Câu hỏi 19: Ai là người đầu tiên lên mặt trăng?",
+        choices: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "CAlan Shepard"],
+        answer: "Neil Armstrong"
+    },
+    {
+        type: "multipleChoice",
+        question: "Câu hỏi 20: Ai là người đầu tiên lên mặt trăng?",
+        choices: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "CAlan Shepard"],
+        answer: "Neil Armstrong"
+    },
+    {
+        type: "multipleAnswer",
+        question: "Câu hỏi 21: Hệ mặt trời không gồm những hành tinh nào?",
+        choices: ["Sao hỏa", "Sao kim", "Lỗ đen", "Trái Đất"],
+        answer: ["Lỗ đen"]
+    },
+    {
+        type: "multipleAnswer",
+        question: "Câu hỏi 22: Hệ mặt trời không gồm những hành tinh nào?",
+        choices: ["Sao hỏa", "Sao kim", "Lỗ đen", "Trái Đất"],
+        answer: ["Lỗ đen"]
+    },
+    {
+        type: "multipleAnswer",
+        question: "Câu hỏi 23: Hệ mặt trời không gồm những hành tinh nào?",
+        choices: ["Sao hỏa", "Sao kim", "Lỗ đen", "Trái Đất"],
+        answer: ["Lỗ đen"]
+    },
+    {
+        type: "multipleAnswer",
+        question: "Câu hỏi 24: Hệ mặt trời không gồm những hành tinh nào?",
+        choices: ["Sao hỏa", "Sao kim", "Lỗ đen", "Trái Đất"],
+        answer: ["Lỗ đen"]
+    },
+    {
+        type: "multipleAnswer",
+        question: "Câu hỏi 25: Hệ mặt trời không gồm những hành tinh nào?",
+        choices: ["Sao hỏa", "Sao kim", "Lỗ đen", "Trái Đất"],
+        answer: ["Lỗ đen"]
+    },
+    {
+        type: "multipleAnswer",
+        question: "Câu hỏi 26: Hệ mặt trời không gồm những hành tinh nào?",
+        choices: ["Sao hỏa", "Sao kim", "Lỗ đen", "Trái Đất"],
+        answer: ["Lỗ đen"]
+    },
+    {
+        type: "multipleAnswer",
+        question: "Câu hỏi 27: Hệ mặt trời không gồm những hành tinh nào?",
+        choices: ["Sao hỏa", "Sao kim", "Lỗ đen", "Trái Đất"],
+        answer: ["Lỗ đen"]
+    },
+    {
+        type: "multipleAnswer",
+        question: "Câu hỏi 28: Hệ mặt trời không gồm những hành tinh nào?",
+        choices: ["Sao hỏa", "Sao kim", "Lỗ đen", "Trái Đất"],
+        answer: ["Lỗ đen"]
+    },
+    {
+        type: "multipleAnswer",
+        question: "Câu hỏi 29: Hệ mặt trời không gồm những hành tinh nào?",
+        choices: ["Sao hỏa", "Sao kim", "Lỗ đen", "Trái Đất"],
+        answer: ["Lỗ đen"]
+    },
+    {
+        type: "multipleAnswer",
+        question: "Câu hỏi 30: Hệ mặt trời không gồm những hành tinh nào?",
+        choices: ["Sao hỏa", "Sao kim", "Lỗ đen", "Trái Đất"],
+        answer: ["Lỗ đen"]
+    },
+    {
+      type: "text",
+      question: "Câu hỏi 31: Giáng sinh là ngày nào ?:",
+      answer: "25/12"
+    },
+    {
+      type: "text",
+      question: "Câu hỏi 32: Giáng sinh là ngày nào ?:",
+      answer: "25/12"
+    },
+    {
+      type: "text",
+      question: "Câu hỏi 33: Giáng sinh là ngày nào ?:",
+      answer: "25/12"
+    },
+    {
+      type: "text",
+      question: "Câu hỏi 34: Giáng sinh là ngày nào ?:",
+      answer: "25/12"
+    },
+    {
+      type: "text",
+      question: "Câu hỏi 35: Giáng sinh là ngày nào ?:",
+      answer: "25/12"
+    },
+    {
+      type: "text",
+      question: "Câu hỏi 36: Giáng sinh là ngày nào ?:",
+      answer: "25/12"
+    },
+    {
+      type: "text",
+      question: "Câu hỏi 37: Giáng sinh là ngày nào ?:",
+      answer: "25/12"
+    },
+    {
+      type: "text",
+      question: "Câu hỏi 38: Giáng sinh là ngày nào ?:",
+      answer: "25/12"
+    },
+    {
+      type: "text",
+      question: "Câu hỏi 39: Giáng sinh là ngày nào ?:",
+      answer: "25/12"
+    },
+    {
+      type: "text",
+      question: "Câu hỏi 40: Giáng sinh là ngày nào ?:",
+      answer: "25/12"
+    }
+  ];
+
+  var questionContainer = document.getElementById("questionContainer");
+
+  questions.forEach(function(questionObj, index) {
+    var questionElement = document.createElement("div");
+    questionElement.classList.add("question");
+
+    var questionText = document.createElement("p");
+    questionText.textContent = questionObj.question;
+    questionElement.appendChild(questionText);
+
+    if (questionObj.type === "trueFalse") {
+      var trueFalseId = "question" + (index + 1) + "True";
+      var trueFalseInput = document.createElement("input");
+      trueFalseInput.type="radio";
+      trueFalseInput.name = "question" + (index + 1);
+      trueFalseInput.id = trueFalseId;
+      trueFalseInput.value = "true";
+      questionElement.appendChild(trueFalseInput);
+
+      var trueFalseLabel = document.createElement("label");
+      trueFalseLabel.textContent = "Đúng";
+      trueFalseLabel.htmlFor = trueFalseId;
+      questionElement.appendChild(trueFalseLabel);
+
+      var trueFalseId = "question" + (index + 1) + "False";
+      var trueFalseInput = document.createElement("input");
+      trueFalseInput.type = "radio";
+      trueFalseInput.name = "question" + (index + 1);
+      trueFalseInput.id = trueFalseId;
+      trueFalseInput.value = "false";
+      questionElement.appendChild(trueFalseInput);
+
+      var trueFalseLabel = document.createElement("label");
+      trueFalseLabel.textContent = "Sai";
+      trueFalseLabel.htmlFor = trueFalseId;
+      questionElement.appendChild(trueFalseLabel);
+    } else if (questionObj.type === "multipleChoice") {
+      questionObj.choices.forEach(function(choice, choiceIndex) {
+        var choiceId = "question" + (index + 1) + "Choice" + (choiceIndex + 1);
+        var choiceInput = document.createElement("input");
+        choiceInput.type = "radio";
+        choiceInput.name = "question" + (index + 1);
+        choiceInput.id = choiceId;
+        choiceInput.value = choice;
+        questionElement.appendChild(choiceInput);
+
+        var choiceLabel = document.createElement("label");
+        choiceLabel.textContent = choice;
+        choiceLabel.htmlFor = choiceId;
+        questionElement.appendChild(choiceLabel);
+      });
+    } else if (questionObj.type === "multipleAnswer") {
+      questionObj.choices.forEach(function(choice, choiceIndex) {
+        var choiceId = "question" + (index + 1) + "Choice" + (choiceIndex + 1);
+        var choiceInput = document.createElement("input");
+        choiceInput.type = "checkbox";
+        choiceInput.name = "question" + (index + 1) + "[]";
+        choiceInput.id = choiceId;
+        choiceInput.value = choice;
+        questionElement.appendChild(choiceInput);
+
+        var choiceLabel = document.createElement("label");
+        choiceLabel.textContent = choice;
+        choiceLabel.htmlFor = choiceId;
+        questionElement.appendChild(choiceLabel);
+      });
+    } else if (questionObj.type === "text") {
+      var textInput = document.createElement("input");
+      textInput.type = "text";
+      textInput.name = "question" + (index + 1);
+      questionElement.appendChild(textInput);
+    }
+
+    questionContainer.appendChild(questionElement);
+  });
+
+  function arraysEqual(arr1, arr2) {
+
+    if (!arr1 || !arr2) {
+        return false;
+    }
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    for (var i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+  function submitQuiz(event) {
+    event.preventDefault();
+
+    var resultElement = document.getElementById("result");
+//   resultElement.textContent  = "Bạn đã trả lời đúng " + correctCount + " câu.";
+
+      var name = localStorage.getItem('name');
+      var dob = localStorage.getItem('dob');
+      var studentId = localStorage.getItem('studentId');
+      var className = localStorage.getItem('className');
+
+     
+      resultElement.innerText  = 'Sinh viên: ' + name    ;
+      resultElement.innerText  += 'Ngày sinh: ' + dob   ;
+      resultElement.innerText  += 'Mã sinh viên: ' + studentId   ;
+      resultElement.innerText  += 'Lớp: ' + className   ;
+
+      var answers = [];
+    var questionElements = document.getElementsByClassName("question");
+
+    for (var i = 0; i < questionElements.length; i++) {
+        var questionElement = questionElements[i];
+        var questionType = questions[i].type;
+
+        if (questionType === "trueFalse") {
+            var selectedAnswer = questionElement.querySelector("input:checked");
+            if (selectedAnswer) {
+                if (selectedAnswer) {
+                    answers.push(selectedAnswer.value === "true");
+                } else {
+                    answers.push("");
+                }
+            }
+        } else if (questionType === "multipleChoice") {
+            var selectedAnswer = questionElement.querySelector("input:checked");
+            if (selectedAnswer) {
+                answers.push(selectedAnswer.value);
+            }else{
+                answers.push("");
+            }
+        } else if (questionType === "multipleAnswer") {
+            var selectedAnswers = [];
+            var selectedCheckboxes = questionElement.querySelectorAll("input:checked");
+            if (selectedCheckboxes) {
+                selectedCheckboxes.forEach(function(checkbox) {
+                    selectedAnswers.push(checkbox.value);
+                });
+            }else{
+                selectedAnswers.push([]);
+            }
+            answers.push(selectedAnswers);
+        } else if (questionType === "text") {
+            var textInput = questionElement.querySelector("input[type='text']");
+            if (textInput) {
+                answers.push(textInput.value);
+            }else{
+                answers.push("");
+            }
+        }
+    }
+
+    // Kiểm tra đáp án và đếm số câu trả lời đúng
+    var correctCount = 0;
+    for (var j = 0; j < questions.length; j++) {
+        var question = questions[j];
+        var userAnswer = answers[j];
+        var correctAnswer = question.answer;
+
+        if (Array.isArray(correctAnswer)) {
+            if (arraysEqual(userAnswer, correctAnswer)) {
+                correctCount++;
+            }
+        } else {
+            if (userAnswer === correctAnswer) {
+                correctCount++;
+            }
+        }
+    }
+ 
+
+    // Hiển thị kết quả
+    resultElement.innerHTML += 'Kết quả: ' + correctCount + ' câu đúng.';
+    resultElement.style.display = 'block';
+
+    return false;
+  }
+
+
+
+
+
+    function saveInputValue() {
+      var name = document.getElementById('name').value;
+      var dob = document.getElementById('dob').value;
+      var studentId = document.getElementById('studentId').value;
+      var className = document.getElementById('class').value;
+
+      localStorage.setItem('name', name);
+      localStorage.setItem('dob', dob);
+      localStorage.setItem('studentId', studentId);
+      localStorage.setItem('className', className);
+
+      document.getElementById('name').value = '';
+      document.getElementById('dob').value = '';
+      document.getElementById('studentId').value = '';
+      document.getElementById('class').value = '';
+
+      var loginForm = document.getElementById('loginForm');
+      loginForm.style.display = 'none';
+
+      var quizContainer = document.getElementById('quizContainer');
+      quizContainer.style.display = 'block';
+
+      return false;
+    }
